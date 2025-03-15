708442432f16b0321566dc388a369ebf3d61ba49
khushveerk <khushveer.prof011@gmail.com>
2025-03-15 09:41:27 +0530

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -397,46 +397,45 @@ public:
     Matrix transpose() { 
         Matrix res; 
         for (int i = 0; i < 3; i++) 
-for (int j = 0; j < 3; j++) 
-res.mat[i][j] = mat[j][i]; 
-return res; 
-} 
-); 
- int main() { 
-Matrix A, B, C; 
-int choice; 
-A.input(); 
-B.input(); 
-do { 
-cout << "\nMenu:\n1. Sum\n2. Product\n3. Transpose\n4. Exit\nEnter choice: "; 
-cin >> choice; 
-switch (choice) { 
-case 1: 
-C=A+B; 
-C.display(); 
-break; 
-case 2: 
-   
-case 3:  
-} 
-} while (choice != 4);  
-return 0; 
-CAB; 
-C.display(); 
-break; 
-C A.transpose(); 
-C.display(); 
-break; 
-case 4: 
-cout << "Exiting...\n"; 
-break; 
-default: 
-cout << "Invalid choice!"; 
-} 
-} while (choice != 4); 
-return 0; 
-}
+            for (int j = 0; j < 3; j++) 
+                res.mat[i][j] = mat[j][i]; 
+        return res; 
+   } 
+}; 
+ 
+int main() { 
+   Matrix A, B, C; 
+   int choice; 
+  
+   A.input(); 
+   B.input(); 
+ 
+   do { 
+      cout << "\nMenu:\n1. Sum\n2. Product\n3. Transpose\n4. Exit\nEnter choice: "; 
+      cin >> choice; 
 
+      switch (choice) { 
+         case 1: 
+            C=A+B; 
+            C.display(); 
+            break; 
+         case 2:
+            C =A*B; 
+            C.display(); 
+            break; 
+         case 3:  
+            C =A.transpose(); 
+            C.display(); 
+            break; 
+         case 4: 
+            cout << "Exiting...\n"; 
+            break; 
+         default: 
+            cout << "Invalid choice!"; 
+      } 
+   } while (choice != 4); 
+   return 0; 
+}
 
 
 
