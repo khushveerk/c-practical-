183f58c8a5d0cc5536749f35dbe47e999b994040
khushveerk <khushveer.prof011@gmail.com>
2025-03-17 10:22:17 +0530

    Update prac1 .cpp

--- prac1 .cpp
+++ prac1 .cpp
@@ -1,1 +1,24 @@
+#include <iostream> 
+#include <vector> 
+#include <set> 
+using namespace std; 
 
+double seriesSum(int n) { 
+   double sum=0; 
+   for (int i =1; i <= n; i++) {  
+      double term 1.0/ pow(1, 1); 
+      if ( i% 2==0) {
+         sum = term; // Alternate terms are negetive 
+      } else { 
+         sum += term;
+      }
+   } 
+   return sum; 
+} 
+int main() {  
+   int n; 
+   cout << "Enter the number of terms: "; 
+   cin>>n; 
+   cout "Sum of series: <<< seriesSum(n) endl;  
+   return 0; 
+}
