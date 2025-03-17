85e79cd128cad48b9374e2fd920ac0fde4f20021
khushveerk <khushveer.prof011@gmail.com>
2025-03-17 12:48:14 +0530

    Update prac1 .cpp

--- prac1 .cpp
+++ prac1 .cpp
@@ -6,7 +6,7 @@ using namespace std;
 double seriesSum(int n) { 
    double sum=0; 
    for (int i =1; i <= n; i++) {  
-      double term =1.0/ pow(i, 1); 
+      double term =1.0/ pow(i, i); 
       if ( i% 2==0) {
          sum -= term; 
       } else { 
