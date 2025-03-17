367b5248f951706ff657e2409daee6b1344709a3
khushveerk <khushveer.prof011@gmail.com>
2025-03-17 10:35:51 +0530

    Create prac11.cpp

---
+++ prac11.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+#include <stdexcept> 
+using namespace std; 
+
+class MatrixException: public exception { 
+public: 
+   const char* what() const throw() { 
+      return "Matrix dimensions are incompatible!"; 
+   } 
+}; 
+
+void checkCompatibility(int rows1, int cols1, int rows2, int cols2) { 
+   if (cols1 != rows2) throw MatrixException(); 
+} 
+
+int main() { 
+   try { 
+      checkCompatibility(2, 6, 4, 2); 
+   } catch (MatrixException e) { 
+      cout << "Eггог: <<<< e.what() << endl; 
+   } 
+   return 0; 
+}   
+
