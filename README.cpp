45808abd0fc3c114b2b78449064a249a8708e813
khushveerk <khushveer.prof011@gmail.com>
2025-03-18 06:02:55 +0530

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -34,18 +34,18 @@ int main() {
 using namespace std; 
 
 void removeDuplicates (vector<int>& arr) { 
-     set<int>uniqueElements(arr.begin(), arr.end()):  
+     set<int> uniqueElements(arr.begin(), arr.end());  
      arr.assign(uniqueElements.begin(), uniqueElements.end()); 
 }
 int main() { 
-     vector<int arr (1, 2, 2, 3, 7, 4, 5, 6, 7); 
+     vector<int> arr {1, 2, 2, 3, 7, 4, 5, 6, 7}; 
 
      cout << "Original array: "; 
      for (int num :arr) cout<<num << " ";
 
      removeDuplicates(arr); 
      cout<< "\nArray after removing duplicates: "; 
-     for (int num arr) cout<< num<<"*; 
+     for (int num : arr) cout<< num<<" "; 
       return 0; 
 }
 
