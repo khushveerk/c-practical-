b740323da4b531b4741da558418c0b2564b0e2d0
khushveerk <khushveer.prof011@gmail.com>
2025-03-18 06:12:26 +0530

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -100,8 +100,8 @@ int main() {
 #include <map> 
   using namespace std; 
 
-void countOccurrences (string str) { 
-   map char, int freq; 
+void countoccurrences (string str) { 
+   map <char, int> freq; 
 
    for (char : str) { 
        if (isalpha(c)) { // Consider only alphabets  
@@ -110,8 +110,8 @@ void countOccurrences (string str) {
    } 
    cout<< "Character Frequency Table:\n"; 
  
-   for (auto pair freq) { 
-       cout<<< pair.first<< " -> " << pair.second << endl; 
+   for (auto& pair freq) { 
+       cout << pair.first<< " -> " << pair.second << endl; 
    }
 }
 int main() { 
@@ -121,7 +121,7 @@ int main() {
 
    countoccurrences(input); 
    return 0; 
-
+}
 
 ##4
 
