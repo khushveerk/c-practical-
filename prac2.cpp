b3b8ccfbb1b4b68de5e8c98afa1b1eeb348a2b2d
khushveerk <khushveer.prof011@gmail.com>
2025-03-24 09:48:24 +0530

    Update prac2.cpp

--- prac2.cpp
+++ prac2.cpp
@@ -1,41 +1,22 @@
-#include <iostream>
-#include <algorithm> 
-using namespace std;
 
-int removeDuplicates(int arr[], int n) {
-    if (n == 0 || n == 1) return n; 
+#include <iostream> 
+#include <vector> 
+#include <set> 
+using namespace std; 
 
-    sort(arr, arr + n); 
-
-    int j = 0; 
-
-   
-    for (int i = 0; i < n - 1; i++) {
-        if (arr[i] != arr[i + 1]) {
-            arr[j++] = arr[i];
-        }
-    }
-    arr[j++] = arr[n - 1]; 
-    return j; 
+void removeDuplicates (vector<int>& arr) { 
+     set<int> uniqueElements(arr.begin(), arr.end());  
+     arr.assign(uniqueElements.begin(), uniqueElements.end()); 
 }
+int main() { 
+     vector<int> arr {1, 2, 2, 3, 7, 4, 5, 6, 7}; 
 
-int main() {
-    int arr[] = {4, 2, 2, 1, 4, 4, 6, 3, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
+     cout << "Original array: "; 
+     for (int num :arr) cout<<num << " ";
 
-    cout << "Original array: ";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-
-    n = removeDuplicates(arr, n); 
-
-    cout << "Array after removing duplicates: ";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-
-    return 0;
+     removeDuplicates(arr); 
+     cout<< "\nArray after removing duplicates: "; 
+     for (int num : arr) cout<< num<<" "; 
+      return 0; 
 }
-
      
