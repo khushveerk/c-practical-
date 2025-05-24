c11f6ea60deb2c7170fb43ad428a4674f922ace1
khushveerk <khushveer.prof011@gmail.com>
2025-05-24 17:01:35 +0530

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -1,6 +1,30 @@
 ##c-practical-
 
 ## 1
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double seriesSum(int n) {
+    double sum = 1.0; 
+    if (n >= 2) {
+        sum -= 1.0 / pow(2, 2); // subtract second term
+    }
+    for (int i = 3; i <= n; i++) {
+        sum += 1.0 / pow(2, i); 
+    }
+    return sum;
+}
+int main() {
+    int n;
+    cout << "Enter the number of terms: ";
+    cin >> n;
+    cout << "Sum of series: " << seriesSum(n) << endl;
+    return 0;
+}
+
+// not this one 
+
 #include <iostream> 
 #include <cmath> 
 using namespace std; 
